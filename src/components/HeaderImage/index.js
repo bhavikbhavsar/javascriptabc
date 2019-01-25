368d1f6ae9459cf92cee28c2270d6f1dcbb8c771
bhavikbhavsar <bhavikbhavsar@users.noreply.github.com>
2019-01-25 14:00:10 +0530

    Update index.js

--- src/components/HeaderImage/index.js
+++ src/components/HeaderImage/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-import Wrapper from './Wrapper';
+//import Wrapper from './Wrapper';
 import imgSrc from '../../main.jpg';
 
 function HeaderImage() {
   return (
-    <Wrapper>
+    //<Wrapper>
       <Link to="/">
         <img src={imgSrc} alt="" />
       </Link>
-    </Wrapper>
+    //</Wrapper>
   );
 }
 
